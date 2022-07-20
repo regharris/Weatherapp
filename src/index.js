efed279107b75fe7b5521db875bf8b72ce464123
reginald <regharris0504@gmail.com>
2022-07-20 12:06:25 -0500

    initialcomit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import './index.css'
+import Loading from './Loading';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+class App extends React.Component {
+  
+     state = {
+          lat: null,
+          long: null,
+          errorMessage: '',
+          date: ''
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude
+                })
+            },
+            (err) => {
+                this.setState({
+                    errorMessage: err.message
+                })
+            }
+        )
+
+    }
+
+
+
+    render() {
+       if (this.state.errorMessage && (!this.state.lat || !this.state.long)) {
+        return <div>Error: {this.state.errorMessage}</div>
+       }
+       if ((this.state.lat || this.state.long) && !this.state.errorMessage) {
+        return(
+              <div>
+                <SeasonDisplay lat={this.state.lat} />
+              </div>
+        )
+        
+       }
+       return <div><Loading /></div>
+    } 
+}
+
+
+ReactDOM.render(<App />,document.querySelector("#root"));
